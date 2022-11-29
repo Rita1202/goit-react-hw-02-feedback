2b6c7f6deaacae30cb7748e84242213f7a02cdc8
Rita1202 <Rita.sokolchenko@gmail.com>
2022-11-29 20:24:06 +0100

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,83 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Statistics } from './Statistics/Statistics';
+import { Component } from 'react';
+import '../index.css';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+  };
+
+  countTotalFeedback = () => {
+    this.setState(prev => ({
+      total: prev.good + prev.neutral + prev.bad,
+    }));
+  };
+
+  changeFeedback = e => {
+    this.setState(prev => ({
+      [e.target.name]: prev[e.target.name] + 1,
+    }));
+    this.countTotalFeedback();
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const res = Math.floor((this.state.good / this.state.total) * 100);
+    return res ? res + '%' : '0%';
+  };
+
+  render() {
+    const { good, neutral, bad, total } = this.state;
+    return (
+      <>
+        <>
+          <p>Please leave your feedback</p>
+
+          <button
+            className="btn"
+            type="button"
+            onClick={this.changeFeedback}
+            name="good"
+            value={this.state.good}
+          >
+            Good
+          </button>
+
+          <button
+            className="btn"
+            type="button"
+            onClick={this.changeFeedback}
+            name="neutral"
+            value={this.state.neutral}
+          >
+            Neutral
+          </button>
+
+          <button
+            className="btn"
+            type="button"
+            onClick={this.changeFeedback}
+            name="bad"
+            value={this.state.bad}
+          >
+            Bad
+          </button>
+
+          {total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <p>No feedback given</p>
+          )}
+        </>
+      </>
+    );
+  }
+}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,9 +9,16 @@ export class Statistics extends Component {
         <p>Good: {this.props.good}</p>
         <p>Neutral: {this.props.neutral}</p>
         <p>Bad: {this.props.bad}</p>
-
         <p>Positive: {this.props.positivePercentage}</p>
       </>
     );
   }
 }
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string,
+};
